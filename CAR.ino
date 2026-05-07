4a1f2b245a93994c03199b6749a2177f26c6a47c
Nandani_solgama08 <240414037124@gandhinagaruni.ac.in>
2026-05-07 19:47:30 +0530

    last edited

--- CAR.ino
+++ CAR.ino
@@ -13,61 +13,74 @@ int IN2 = 8;
 //MOTOR2
 int ENB = 5;
 int IN3 = 7;
-int IN4 = 12; //6 par thi
+int IN4 = 12;  //6 par thi
 
 //servo pin
 int S1 = 11;
 
 //speed
-int Speed_m1 = 90;
-int Speed_m2 = 100;
-int Speed_l1 = 160;
-int Speed_l2 = 170;
-int Speed_h1 = 245;
-int Speed_h1 = 255;
+int Speed_m1 = 160;
+int Speed_m2 = 170;
+int Speed_l1 = 90;
+int Speed_l2 = 90;
+int Speed_h1 = 200;
+int Speed_h2 = 200;
 
+
+int toggle = 0;
 //SPEED SET OF TWO MOTOTR
 int M1_s = Speed_m1;
 int M2_s = Speed_m2;
+void delay_forward(){
+  forward();
+  delay(500);
+  stop();
+}
 
+void delay_left(){
+  left();
+  delay(500);
+  stop();
+}
+void delay_right(){
+  right();
+  delay(500);
+  stop();
+}
 void forward() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  analogWrite(ENA, Speed_l1);
-  analogWrite(ENB, Speed_l2);
+  analogWrite(ENA, 140);
+  analogWrite(ENB, 140);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  Serial.println("Forward");
 }
 
 void backward() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
+  analogWrite(ENA, 130);
+  analogWrite(ENB, 145);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void left() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
   analogWrite(ENA, Speed_l1);
   analogWrite(ENB, Speed_l2);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  Serial.println("Backward");
-}
-
-void left() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  analogWrite(ENA, M1_s);
-  analogWrite(ENB, M2_s);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  Serial.println("LEFT");
 }
 
 void right() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  analogWrite(ENA, M1_s);
-  analogWrite(ENB, M2_s);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, Speed_l1);
+  analogWrite(ENB, Speed_l2);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  Serial.println("RIGHT");
 }
 
 void stop() {
@@ -77,10 +90,10 @@ void stop() {
   analogWrite(ENB, 0);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
+
 }
 
 void setup() {
-  Serial.begin(9600);
   Dabble.begin(9600);
 
   pinMode(ENA, OUTPUT);
@@ -88,12 +101,26 @@ void setup() {
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
+
   pinMode(IN4, OUTPUT);
+}
+
+void set_toggle(){
+  if (toggle == 0){
+    toggle = 1;
+  }else{
+    toggle = 0;
+  }
 }
 
 void loop() {
 
   Dabble.processInput();
+  if (GamePad.isCirclePressed()){
+    set_toggle();
+  }
+
+
   if (GamePad.isUpPressed()) {
     forward();
   } else if (GamePad.isDownPressed()) {
@@ -107,28 +134,20 @@ void loop() {
   }
 
   //GRIPPER CONTROL
-  if (GamePad.isSquarePressed()) {
-    gripper.attach(S1);
-    gripper.write(180);
-    delay(1000);
-    gripper.detach();
-  } else {
+  if (GamePad.isSelectPressed()) {
     gripper.attach(S1);
     gripper.write(0);
     delay(1000);
     gripper.detach();
+  } else if (GamePad.isStartPressed()) {
+    gripper.attach(S1);
+    gripper.write(150);
+    delay(1000);
+    gripper.detach();
   }
 
-
-  // //speed control
-  if (GamePad.isStartPressed()) {
-    M1_s = Speed_l;
-    M2_s = Speed_l;
-  } else if (GamePad.isSelectPressed()) {
-    M1_s = 160;
-    M2_s = 0;
-  } else if (GamePad.isCrossPressed()) {
-    M1_s = Speed_h;
-    M2_s = Speed_h;
+  if (GamePad.isCrossPressed()) {
+    stop();
   }
+
 }
